fab17634377c2e0865ccf853f562548bd200f016
Stanley Stout <stouts@mac.com>
2016-07-25 14:31:16 -0500

    UI done for real this time

---
+++ College Profile Builder/College.swift
@@ -0,0 +1,13 @@
+//
+//  College.swift
+//  College Profile Builder
+//
+//  Created by Stanley Stout on 7/25/16.
+//  Copyright © 2016 Chris Stout. All rights reserved.
+//
+
+import UIKit
+
+class College: NSObject {
+
+}
