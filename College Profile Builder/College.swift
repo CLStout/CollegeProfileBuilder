587774a4f7cdbe66dd658af28f15a30460bc53de
Stanley Stout <stouts@mac.com>
2016-07-25 20:43:38 -0500

    Custom class and about half of the main view because I'm forgetful and forgot to do this earlier, oops I tried/

--- College Profile Builder/College.swift
+++ College Profile Builder/College.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 class College: NSObject {
-
+    var name = ""
+    var location = ""
+    var enrollment = 0
+    var image = UIImage(named: "Default")
+    
+    convenience init(name: String, location: String, enrollment: Int, image: UIImage) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.enrollment = enrollment
+        self.image = image
+    }
+    convenience init(name: String) {
+        self.init()
+        self.name = name
+    }
 }
--- College Profile Builder/ViewController.swift
+++ College Profile Builder/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    var colleges : [College] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        editButton.tag = 0
+        colleges.append(College(name: "Northwestern", location: "IL", enrollment: 20336, image: UIImage(named: "Northwestern")!))
+        colleges.append(College(name: "Harvey Mudd", location: "CA", enrollment: 735, image: UIImage(named: "HarveyMudd")!))
+        colleges.append(College(name: "Mossouri S&T", location: "MO", enrollment: 8889, image: UIImage(named: "MissouriST")!))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
-
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colleges.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        cell.textLabel?.text = colleges[indexPath.row].name
+        return cell
+    }
+    
+    @IBAction func onTappedPlusButton(sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField) in textField.placeholder = "Add College Here" }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (action) in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(College(name: collegeTextField.text!))
+            self.tableView.reloadData()
+        }
+        alert.addAction(addAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
